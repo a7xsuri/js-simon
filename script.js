4aedea489fd19ccd27c4e4f14ed4cac258f1cdf9
a7xsuri <leonardofilipposurico@gmail.com>
2022-04-06 20:58:56 +0200

    add verificationNumber

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-SetTimeout(RandomGenerateNumbers(){
-    const NumeberTogenerate = 5;
+const NumeberTogenerate = 5;
+let Row = document.querySelector('.riga');
+function RandomGenerateNumbers(){
     let NumberGenerate=[];
     for(i=0;i<NumeberTogenerate;i++){
         let GenerateNumber = Math.floor(Math.random() * 100);
         NumberGenerate.push(GenerateNumber);
     }
-    let Row = document.querySelector('.riga');
     Row.innerHTML += NumberGenerate;
     return NumberGenerate
-}
+};
 
+RandomGenerateNumbers();
+setTimeout(VerificationNumber, 1000)
+
+function VerificationNumber(){
+Row.style.display= "none";
+const NewRow = document.createElement("p");
+let NumRight =[];
+for(i=0;i<NumeberTogenerate;i++){
+    let Verifica = prompt('inserisci il numero');
+    if(Verifica.includes(NumberGenerate)){
+        NumRight.push(Verifica);
+    }
+}
+NewRow.innerHTML = "i numeri indovinati sono" + NumRight;
+console.log(Verifica)
+}
